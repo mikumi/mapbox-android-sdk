5ea4d877ff276f27756a74b4df0e53e659053003
Martin Guillon <martin.guillon@akylas.fr>
2014-09-12 19:31:49 +0200

    prevent crash!

--- MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/views/util/Projection.java
+++ MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/views/util/Projection.java
@@ -431,6 +431,10 @@ public class Projection implements GeoConstants {
     public static LatLng pixelXYToLatLong(double pixelX, double pixelY, final float levelOfDetail) {
         final double mapSize = mapSize(levelOfDetail);
         final double maxSize = mapSize - 1.0;
+        if (mapSize < maxSize) {
+            return null;
+        }
+        
         double x = wrap(pixelX, 0, maxSize, mapSize);
         double y = wrap(pixelY, 0, maxSize, mapSize);
 
